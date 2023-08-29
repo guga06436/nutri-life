3a5a99205b092b676efa20d3fa52799aaf261ace
Ryann <ryann.arruda_159@hotmail.com>
2023-08-29 02:16:31 -0300

    Criação das classes de persistência de dados
    Cada entidade está associada a sua classe de persistência, a qual irá armazenar dados em memória RAM de instâncias específicas

---
+++ nutri-life/src/persistence/NutritionistPersistence.java
@@ -0,0 +1,10 @@
+package persistence;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import entities.Nutritionist;
+
+public class NutritionistPersistence {
+	Set<Nutritionist> nutritionistPersistence = new HashSet<>();
+}
---
+++ nutri-life/src/persistence/PatientPersistence.java
@@ -0,0 +1,10 @@
+package persistence;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import entities.Patient;
+
+public class PatientPersistence {
+	Set<Patient> patientPersistence = new HashSet<>();
+}
