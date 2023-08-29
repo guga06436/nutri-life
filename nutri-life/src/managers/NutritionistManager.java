45ebec1a05449f4cab540789c7cf9a220ae4a5d4
Ryann <ryann.arruda_159@hotmail.com>
2023-08-29 02:03:47 -0300

    Definição dos métodos que os gerenciadores das entidades devem possuir

---
+++ nutri-life/src/managers/NutritionistManager.java
@@ -0,0 +1,8 @@
+package managers;
+
+import entities.Nutritionist;
+
+public interface NutritionistManager {
+	boolean add(Nutritionist n);
+	void listAll();
+}
---
+++ nutri-life/src/managers/PatientManager.java
@@ -0,0 +1,8 @@
+package managers;
+
+import entities.Patient;
+
+public interface PatientManager {
+	boolean add(Patient n);
+	void listAll();
+}
