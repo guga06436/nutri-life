2a5d65f05857d9a11884f7505519db6f823f13ca
diegolrs <diegosilva@eng.ci.ufpb.br>
2023-08-31 14:23:01 -0300

    option handler now read inputs

--- nutri-life/src/application/OptionHandler.java
+++ nutri-life/src/application/OptionHandler.java
@@ -1,8 +1,26 @@
 package application;
+import java.util.Scanner;
 
 public class OptionHandler
 {
-    public static void showInputDialog(String data)
+    static Scanner in = new Scanner(System.in);
+
+    public static String readStringInput()
+    {
+        return in.next();
+    }
+
+    public static String readLineInput()
+    {
+        return in.nextLine();
+    }
+
+    public static int readIntegerInput()
+    {
+        return in.nextInt();
+    }
+
+    public static void showMessage(String data)
     {
         System.out.println(data);
     }
