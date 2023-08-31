d4bdd1e366f7467f809821b5af81edfaf87a58ec
Alexandre Bezerra de Lima <53580956+Alexandreprog@users.noreply.github.com>
2023-08-31 09:41:51 -0300

    Refatoração de método
    Refatoração do método de adicionar

--- nutri-life/src/controler/impl/NutritionistManagerImpl.java
+++ nutri-life/src/controler/impl/NutritionistManagerImpl.java
@@ -1,24 +1,18 @@
 package controler.impl;
 
-import model.Nutritionist;
 import controler.NutritionistManager;
+import model.Nutritionist;
 import persistence.NutritionistPersistence;
 
 public class NutritionistManagerImpl implements NutritionistManager{
-
+	private NutritionistPersistence np;
+	
 	public NutritionistManagerImpl() {
-
+		np = new NutritionistPersistence();
 	}
 	
 	@Override
 	public boolean add(Nutritionist n) {
-		return NutritionistPersistence.nutritionistPersistence.add(n);
-	}
-
-	@Override
-	public void listAll() {
-		for(Nutritionist n: NutritionistPersistence.nutritionistPersistence) {
-			System.out.println(n);
-		}
+		return np.add(n);
 	}
 }
