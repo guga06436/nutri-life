0fa72a4ebd47754e8c9d3da12fde048b9e762ec6
Alexandre Bezerra de Lima <53580956+Alexandreprog@users.noreply.github.com>
2023-08-31 09:23:13 -0300

    Novo método
    Método de adicionar nutricionista

--- nutri-life/src/persistence/NutritionistPersistence.java
+++ nutri-life/src/persistence/NutritionistPersistence.java
@@ -1,10 +1,41 @@
 package persistence;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
 
 import model.Nutritionist;
+import persistence.db.Database;
 
 public class NutritionistPersistence {
-	public static Set<Nutritionist> nutritionistPersistence = new HashSet<>();
+	private Connection conn;
+	
+	public NutritionistPersistence() {
+		conn = Database.getConnection();
+	}
+	
+	public boolean add(Nutritionist n) {
+		PreparedStatement ps = null;
+		int rowsAffected = -1;
+		
+		try {
+			ps = (PreparedStatement) conn.prepareStatement("INSERT INTO Nutricionist(name, age, crn) VALUES(?,?,?)");
+			
+			ps.setString(1, n.getName());
+			ps.setInt(2, n.getAge());
+			ps.setString(3, n.getCrn());
+			
+			rowsAffected = ps.executeUpdate();
+			
+			if(rowsAffected > 0) {
+				return true;
+			}
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
 }
