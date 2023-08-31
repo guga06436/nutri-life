e534c21570ee6b0474a92b83f6180c28482d5669
diegolrs <diegosilva@eng.ci.ufpb.br>
2023-08-31 14:23:33 -0300

    main now just runs application

--- nutri-life/src/Main.java
+++ nutri-life/src/Main.java
@@ -5,7 +5,7 @@ public class Main
     public static void main(String[] args)
     {
         MainScreenDesktop application = new MainScreenDesktop();
-        application.showMenu();
+        application.run();
         //System.out.println(a.toString());
     }
 }
