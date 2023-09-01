1b3517cfbddf7fc20826f5861d875929fddf9e35
Alexandre Bezerra de Lima <53580956+Alexandreprog@users.noreply.github.com>
2023-08-31 22:58:49 -0300

    Remoção de atributo birthdate desnecessário e inclusão de verificações ausentes

--- nutri-life/src/views/PatientFormView.java
+++ nutri-life/src/views/PatientFormView.java
@@ -1,21 +1,17 @@
 package views;
 
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import controler.PatientManager;
 import controler.impl.PatientManagerImpl;
-import model.Patient;
 import exceptions.ExceptionLogin;
-import exceptions.ExceptionPassword;
 import exceptions.ExceptionNotFound;
+import exceptions.ExceptionPassword;
 import exceptions.ExceptionRegister;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-
-import java.util.Date;
-import java.util.List;
-import java.util.Scanner;
+import model.Patient;
 
 public class PatientFormView {
 
@@ -25,8 +21,6 @@ public class PatientFormView {
     public PatientFormView(){
         patientManager = new PatientManagerImpl();
     }
-
-
 
     public void run() throws ExceptionRegister {
 
@@ -63,6 +57,40 @@ public class PatientFormView {
         }
     }
 
+    public void view() throws ExceptionRegister {
+
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+            System.out.println("[1] Sign In");
+            System.out.println("[2] Register");
+            System.out.println("[3] Exit");
+            System.out.print("Choose an option: ");
+            int option = sc.nextInt();
+            sc.nextLine();
+
+            switch (option) {
+                case 1:
+                    try {
+                        signIn(sc);
+                    } catch (ExceptionLogin | ExceptionPassword e) {
+                        System.out.println("Login failed: " + e.getMessage());
+                    } catch (ExceptionNotFound e){
+                        System.out.println("Login failed: Patient not found");
+                    }
+                    break;
+                case 2:
+                    register(sc);
+                    break;
+                case 3:
+                    System.out.println("Exiting...");
+                    sc.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid option");
+            }
+        }
+    }
 
     private static void signIn(Scanner scanner) throws ExceptionLogin, ExceptionPassword, ExceptionNotFound {
 
@@ -78,16 +106,6 @@ public class PatientFormView {
 
     }
 
-
-    private static Date parseBirthdate(String birthdate) {
-        try {
-            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-            return dateFormat.parse(birthdate);
-        } catch (ParseException e) {
-            return null;
-        }
-    }
-
     private static void register(Scanner scanner) throws ExceptionRegister {
 
         System.out.print("Name: ");
@@ -96,16 +114,9 @@ public class PatientFormView {
         System.out.print("Age: ");
         int age = scanner.nextInt();
         scanner.nextLine();
-
-        System.out.print("Birthdate: ");
-        String birthdate = scanner.nextLine();
-
-        // Convert String to Date
-        Date date = parseBirthdate(birthdate);
-        if (date == null) {
-            System.out.println("Invalid birthdate format");
-            return;
-        }
+        
+        System.out.print("CPF: ");
+        String cpf = scanner.nextLine();
 
         System.out.print("Height: ");
         float height = scanner.nextFloat();
@@ -114,8 +125,8 @@ public class PatientFormView {
         float weight = scanner.nextFloat();
         scanner.nextLine();
 
-        System.out.print("Login: ");
-        String login = scanner.nextLine();
+        System.out.print("Username: ");
+        String username = scanner.nextLine();
 
         System.out.print("Password: ");
         String password = scanner.nextLine();
@@ -123,8 +134,33 @@ public class PatientFormView {
         if (password.length() < 8 || password.length() > 20) {
             throw new ExceptionRegister("Password lenght must be between 8 and 20.");
         }
+        
+        Pattern pattern = Pattern.compile("\\d");
+        Matcher matcher = pattern.matcher(password);
+        
+        int count = 0;
+        while (matcher.find()) {
+            count++;
+        }
+        
+        if(count < 2) {
+        	throw new ExceptionRegister("The password must have at least 2 numbers");
+        }
+        
+        boolean containsLetters = false;
 
-        Patient newPatient = new Patient(login, password, name, age, date, height, weight);
+        for (char c : password.toCharArray()) {
+            if (Character.isLetter(c)) {
+                containsLetters = true;
+                break; // Se encontrar uma letra, sai do loop
+            }
+        }
+
+        if (!containsLetters) {
+            throw new ExceptionRegister("The password msut have at least 1 letter");
+        } 
+
+        Patient newPatient = new Patient(username, password, name, cpf, age, height, weight);
         boolean registerSuccess = patientManager.add(newPatient);
 
         /*Condição de Sucesso*/
@@ -134,11 +170,11 @@ public class PatientFormView {
             System.out.println("Failed" + newPatient.getName());
         }
 
-        if (login.length() > 12) {
+        if (username.length() > 12) {
             throw new ExceptionRegister("Login must not be of length above 12.");
-        } else if (login.isEmpty()) {
+        } else if (username.isEmpty()) {
             throw new ExceptionRegister("Login must not be empty.");
-        } else if (login.matches(".*\\d.*")) {
+        } else if (username.matches(".*\\d.*")) {
             throw new ExceptionRegister("Login must not contain numbers.");
         }
     }
