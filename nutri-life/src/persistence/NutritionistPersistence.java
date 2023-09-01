1e3e68c52511b640d7f8a568f5bd96af8e61d8dd
Ryann <ryann.arruda_159@hotmail.com>
2023-08-31 22:19:30 -0300

    Criação do código para manipulação do banco de dados relativo ao Paciente

--- nutri-life/src/persistence/NutritionistPersistence.java
+++ nutri-life/src/persistence/NutritionistPersistence.java
@@ -1,11 +1,44 @@
 package persistence;
-
-import java.util.HashSet;
-import java.util.Set;
-
+import java.sql.SQLException;
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
 import model.Nutritionist;
+import persistence.db.Database;
 
 public class NutritionistPersistence {
-	public static Set<Nutritionist> nutritionistPersistence = new HashSet<>();
-}
+	private Connection conn;
+	
+	public NutritionistPersistence() {
+		conn = Database.getConnection();
+	}
+	
+	public boolean add(Nutritionist n) {
+		PreparedStatement ps = null;
+		int rowsAffected = -1;
 
+		try {
+			ps = (PreparedStatement) conn.prepareStatement("INSERT INTO Nutricionist(name, age, crn, username, password)" + 
+															" VALUES(?,?,?,?,?)");
+
+			ps.setString(1, n.getName());
+			ps.setInt(2, n.getAge());
+			ps.setString(3, n.getCrn());
+			ps.setString(4, n.getUsername());
+			ps.setString(5, n.getPassword());
+
+			rowsAffected = ps.executeUpdate();
+
+			if(rowsAffected > 0) {
+				return true;
+			}
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			Database.closeStatement(ps);
+		}
+		
+		return false;
+	}
+}
