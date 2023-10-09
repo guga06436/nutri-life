ffc0369d8968558c8161b824dc37271369ec694b
Alexandre Bezerra de Lima <53580956+Alexandreprog@users.noreply.github.com>
2023-10-09 14:35:41 -0300

    Add files via upload

--- nutri-life/src/controller/NutritionistManager.java
+++ nutri-life/src/controller/NutritionistManager.java
@@ -3,9 +3,10 @@ package controller;
 import controller.exceptions.ExceptionNotFound;
 import controller.exceptions.ExceptionPassword;
 import model.Nutritionist;
+import persistence.db.exception.InfraException;
 
 public interface NutritionistManager {
-	boolean add(Nutritionist n);
+	boolean add(Nutritionist n) throws InfraException;
 
 	Nutritionist retrieve(String login, String password) throws ExceptionNotFound, ExceptionPassword;
 }
