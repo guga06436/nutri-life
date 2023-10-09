e3594fb476dccd9a90d40239e942c01c51925aa2
Ryann <ryann.arruda_159@hotmail.com>
2023-10-09 13:33:00 -0300

    Troca do tratamento das exceções, realizando a propagação

--- nutri-life/src/controller/impl/PatientManagerImpl.java
+++ nutri-life/src/controller/impl/PatientManagerImpl.java
@@ -3,7 +3,6 @@ package controller.impl;
 import java.util.List;
 
 import controller.PatientManager;
-import controller.exceptions.DatabaseException;
 import model.Patient;
 import persistence.PatientPersistence;
 import persistence.db.exception.InfraException;
@@ -11,36 +10,21 @@ import persistence.db.exception.InfraException;
 public class PatientManagerImpl implements PatientManager{
 	private PatientPersistence pp;
 	
-	public PatientManagerImpl() {
-		try {
-			pp = new PatientPersistence();
-		}
-		catch(InfraException e) {
-			throw new DatabaseException("Could not connect to the database."); 
-		}
+	public PatientManagerImpl() throws InfraException {
+		pp = new PatientPersistence();
 	}
 	
 	@Override
-	public boolean add(Patient n) {
-		try {
-			return pp.add(n);
-		}
-		catch(InfraException e) {
-			throw new DatabaseException("Unable to create a patient.");
-		}
+	public boolean add(Patient n) throws InfraException {
+		return pp.add(n);
 	}
 
 	@Override
-	public void listAll() {
-		try {
-			List<Patient> patients = pp.listAll();
-			
-			for(Patient p : patients) {
-				System.out.println(p);
-			}
-		}
-		catch(InfraException e) {
-			throw new DatabaseException("Unable to show patients.");
+	public void listAll() throws InfraException {
+		List<Patient> patients = pp.listAll();
+		
+		for(Patient p : patients) {
+			System.out.println(p);
 		}
 	}
 
