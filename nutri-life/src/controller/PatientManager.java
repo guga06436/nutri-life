123c2ae7a9b038f8e9541a2817a6c55bfb12341d
Ryann <ryann.arruda_159@hotmail.com>
2023-10-09 13:35:21 -0300

    Alteração na assinatura dos métodos, adicionando cláusula de propagação de exceção

--- nutri-life/src/controller/PatientManager.java
+++ nutri-life/src/controller/PatientManager.java
@@ -1,9 +1,10 @@
 package controller;
 
 import model.Patient;
+import persistence.db.exception.InfraException;
 
 public interface PatientManager {
-	boolean add(Patient n);
-	void listAll();
+	boolean add(Patient n) throws InfraException;
+	void listAll() throws InfraException;
 	Patient retrieve(String login, String password);
 }
