eedf3f309ffe9d4288c4c615b385db6458b0dd78
Gustavo <guga06436@gmail.com>
2023-10-13 18:56:56 -0300

    Add Main

---
+++ nutri-life/src/Main.java
@@ -0,0 +1,10 @@
+import views.MainScreenDesktop;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        MainScreenDesktop application = new MainScreenDesktop();
+        application.run();
+    }
+}
