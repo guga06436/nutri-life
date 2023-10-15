bbeed2d651c577348bcf085f80cde04a2e257d3d
Gustavo <guga06436@gmail.com>
2023-10-15 12:31:02 -0300

    Creation of MealPlan Form

--- nutri-life/src/controller/MealPlanManager.java
+++ nutri-life/src/controller/MealPlanManager.java
@@ -1,12 +1,12 @@
 package controller;
 
 import controller.exceptions.ExceptionMealPlan;
+import model.Patient;
 
 public interface MealPlanManager {
     void createMealPlan(String planName, String goals, List<Recipe> recipeList) throws ExceptionMealPlan;
     void updateMealPlan(MealPlan mealPlan, String planName, String goals, List<Recipe> recipeList) throws ExceptionMealPlan;
     void deleteMealPlan(MealPLan mealPLan) throws ExceptionMealPlan;
-    void getAllMealPlans();
-
+    void viewMealPlan(Patient patient);
 
 }
