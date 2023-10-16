2c9a98ca6055cccc2cea8bd54a0eacc8dbaa063a
Ryann <ryann.arruda_159@hotmail.com>
2023-10-16 01:00:42 -0300

    Adição do método 'addNewRecipe'
    Criação do método que permite o usuário nutricionista adicione um novo ingrediente/alimento as receitas existentes

--- nutri-life/src/model/MealPlan.java
+++ nutri-life/src/model/MealPlan.java
@@ -1,3 +1,5 @@
+package model;
+
 import lombok.Data;
 
 import java.util.Date;
@@ -5,16 +7,19 @@ import java.util.List;
 
 @Data
 public class MealPlan {
-    private String planName; // Nome do plano (até 60 posições alfanuméricas)
-    private Date creationDate; // Data de criação
-    private String goals; // Objetivos
-    private List<Recipe> recipeList; // Lista de receitas
+    private String name;
+    private Date creationDate;
+    private String goals; 
+    private List<Recipe> recipeList;
 
-    public MealPlan(String planName, Date creationDate, String goals, List<Recipe> recipeList) {
-        this.planName = planName;
+    public MealPlan(String name, Date creationDate, String goals, List<Recipe> recipeList) {
+        this.name = name;
         this.creationDate = creationDate;
         this.goals = goals;
         this.recipeList = recipeList;
     }
-
+    
+    public void addNewRecipe(Recipe recipe) {
+    	recipeList.add(recipe);
+    }
 }
