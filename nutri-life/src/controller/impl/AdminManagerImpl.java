636433588b7c923315a6fcf01a635fd3417130bf
Ryann <ryann.arruda_159@hotmail.com>
2023-10-17 02:40:35 -0300

    Implementação base do controller Admin

--- nutri-life/src/controller/impl/AdminManagerImpl.java
+++ nutri-life/src/controller/impl/AdminManagerImpl.java
@@ -1,28 +1,87 @@
 package controller.impl;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import controller.AdminManager;
 import controller.exceptions.ExceptionNotFound;
 import controller.exceptions.ExceptionPassword;
+import controller.exceptions.ExceptionRegister;
 import model.Admin;
+import persistence.Persistence;
 import persistence.db.exception.InfraException;
-import persistence.impl.AdminPersistence;
+import persistence.impl.FactoryAdmin;
 
 public class AdminManagerImpl implements AdminManager {
-    private AdminPersistence ap;
+	private final static FactoryAdmin fa = new FactoryAdmin();
+	private static Persistence<Admin> persistence;
 
     public AdminManagerImpl() throws InfraException {
-        ap = new AdminPersistence();
+    	persistence = fa.getPersistence();
     }
+    
+	private void validateUsername(String username) throws ExceptionRegister {
+		if (username.length() > 12) {
+			throw new ExceptionRegister("Login must not be longer than 12 characters.");
+		} else if (username.isEmpty()) {
+			throw new ExceptionRegister("Login must not be empty.");
+		} else if (username.matches(".*\\d.*")) {
+			throw new ExceptionRegister("Login must not contain numbers.");
+		}
+	}
+
+	private void validatePassword(String password) throws ExceptionRegister {
+		if (password.length() < 8 || password.length() > 20) {
+			throw new ExceptionRegister("Password length must be between 8 and 20.");
+		}
+
+		Pattern pattern = Pattern.compile("\\d");
+		Matcher matcher = pattern.matcher(password);
+
+		int count = 0;
+		while (matcher.find()) {
+			count++;
+		}
+
+		if (count < 2) {
+			throw new ExceptionRegister("The password must have at least 2 numbers.");
+		}
+
+		boolean containsLetters = false;
+		for (char c : password.toCharArray()) {
+			if (Character.isLetter(c)) {
+				containsLetters = true;
+				break;
+			}
+		}
+
+		if (!containsLetters) {
+			throw new ExceptionRegister("The password must have at least 1 letter.");
+		}
+	}
 
     @Override
-    public boolean add(Admin admin) throws InfraException {
-        return ap.add(admin);
+	public boolean insert(String name, String email, String username, String password) throws InfraException, ExceptionRegister {
+		validateUsername(username);
+		validatePassword(password);
+		
+		Admin admin = new Admin(name, email, username, password);
+		return persistence.insert(admin);
     }
 
     @Override
     public Admin retrieve(String username, String password) throws ExceptionNotFound, ExceptionPassword, InfraException {
-        //return ap.retrieve(login, password);
-        return null;
+    	Admin admin = new Admin();
+    	admin.setUsername(username);
+    	admin.setPassword(password);
+    	
+        Admin aux = persistence.retrieve(admin);
+        
+        if(aux == null) {
+        	throw new ExceptionNotFound("Admin not found");
+        }
+        
+        return aux;
     }
 
     @Override
