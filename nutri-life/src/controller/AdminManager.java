080874d65c0413055eb44d28eaacff0533a205b6
Alexandre Bezerra de Lima <53580956+Alexandreprog@users.noreply.github.com>
2023-10-17 02:27:56 -0300

    Adição de nova exceção propagada

--- nutri-life/src/controller/AdminManager.java
+++ nutri-life/src/controller/AdminManager.java
@@ -2,11 +2,12 @@ package controller;
 
 import controller.exceptions.ExceptionNotFound;
 import controller.exceptions.ExceptionPassword;
+import controller.exceptions.ExceptionRegister;
 import model.Admin;
 import persistence.db.exception.InfraException;
 
 public interface AdminManager {
-    boolean insert(String name, String email, String username, String password) throws InfraException;
+    boolean insert(String name, String email, String username, String password) throws InfraException, ExceptionRegister;
     Admin retrieve(String username, String password) throws ExceptionNotFound, ExceptionPassword, InfraException;
     void generateReport() throws InfraException;
 }
