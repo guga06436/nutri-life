366b4a1a2f4be0c2fbbb1b29be0af4c2f60729e3
Alexandre Bezerra de Lima <53580956+Alexandreprog@users.noreply.github.com>
2023-10-17 03:16:39 -0300

    Alteração do parâmetro do método retrieve

--- nutri-life/src/controller/NutritionistManager.java
+++ nutri-life/src/controller/NutritionistManager.java
@@ -9,5 +9,5 @@ import persistence.db.exception.InfraException;
 public interface NutritionistManager {
 	boolean add(String name, int age, String crn, String username, String password) throws InfraException, ExceptionRegister;
 
-	Nutritionist retrieve(String login, String password) throws ExceptionNotFound, ExceptionPassword, InfraException;
+	Nutritionist retrieve(String username, String password) throws ExceptionNotFound, ExceptionPassword, InfraException;
 }
