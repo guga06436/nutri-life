1b9cf2692ea72fd57c9a10ae7e0c589f717f8364
Ryann <ryann.arruda_159@hotmail.com>
2023-10-17 03:41:40 -0300

    Adição do método listAll

--- nutri-life/src/persistence/Persistence.java
+++ nutri-life/src/persistence/Persistence.java
@@ -1,8 +1,13 @@
 package persistence;
 
+import java.util.List;
+
+import persistence.db.exception.InfraException;
+
 public interface Persistence<T> {
-	boolean insert(T object);
-	T retrieve(T object);
-	boolean update(T object);
-	T delete(T object);
+	boolean insert(T object) throws InfraException;
+	T retrieve(T object) throws InfraException;
+	boolean update(T object) throws InfraException;
+	T delete(T object) throws InfraException;
+	List<T> listAll() throws InfraException;
 }
