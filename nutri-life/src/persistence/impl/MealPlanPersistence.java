70db157cf279874e2b976c705e7bd556c131cf2e
Alexandre Bezerra de Lima <53580956+Alexandreprog@users.noreply.github.com>
2023-10-17 03:46:41 -0300

    Adição do método listAll

--- nutri-life/src/persistence/impl/MealPlanPersistence.java
+++ nutri-life/src/persistence/impl/MealPlanPersistence.java
@@ -4,12 +4,14 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Date;
+import java.util.List;
 
 import model.MealPlan;
+import persistence.Persistence;
 import persistence.db.Database;
 import persistence.db.exception.InfraException;
 
-public class MealPlanPersistence {
+public class MealPlanPersistence implements Persistence<MealPlan>{
 	private static Connection conn;
 	
 	public MealPlanPersistence() throws InfraException {
@@ -19,12 +21,42 @@ public class MealPlanPersistence {
 	private MealPlan instantiateMealPlan(ResultSet rs) throws SQLException{
 		MealPlan mealPlan = new MealPlan();
 		
-		mealPlan.setName(rs.getString("mealplan_name"));
+		mealPlan.setPlanName(rs.getString("mealplan_name"));
 		mealPlan.setCreationDate(new Date(rs.getTimestamp("date_creation").getTime()));
 		mealPlan.setGoals(rs.getString("goals"));
 		
 		// Criar lista de receitas do MealPlan
 
 		return mealPlan;
+	}
+
+	@Override
+	public boolean insert(MealPlan object) throws InfraException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public MealPlan retrieve(MealPlan object) throws InfraException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean update(MealPlan object) throws InfraException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public MealPlan delete(MealPlan object) throws InfraException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<MealPlan> listAll() throws InfraException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
