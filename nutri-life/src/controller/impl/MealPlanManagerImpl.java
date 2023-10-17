a2d3fbc7d8d077d95bb514a966dad53c01dc5179
Alexandre Bezerra de Lima <53580956+Alexandreprog@users.noreply.github.com>
2023-10-17 03:09:21 -0300

    Add files via upload
    Adição das dependências da fábrica e reformulação da lógica da persistência

--- nutri-life/src/controller/impl/MealPlanManagerImpl.java
+++ nutri-life/src/controller/impl/MealPlanManagerImpl.java
@@ -1,20 +1,49 @@
 package controller.impl;
 
-import lombok.Data;
 import java.util.Date;
 import java.util.List;
 
-@Data
-public class MealPlanManagerImpl implements MealPlan{
-    private String planName;
-    private Date creationDate;
-    private String goals;
-    private List<Recipe> recipeList;
+import controller.MealPlanManager;
+import controller.exceptions.ExceptionMealPlan;
+import lombok.Data;
+import model.MealPlan;
+import model.Patient;
+import model.Recipe;
+import persistence.Persistence;
+import persistence.db.exception.InfraException;
+import persistence.impl.FactoryMealPlan;
 
-    public MealPlanManagerImpl(String planName, Date creationDate, String goals, List<Recipe> recipeList) {
-        this.planName = planName;
-        this.creationDate = creationDate;
-        this.goals = goals;
-        this.recipeList = recipeList;
-    }
+@Data
+public class MealPlanManagerImpl implements MealPlanManager{
+	private static FactoryMealPlan fmp;
+	private static Persistence<MealPlan> persistence;
+	
+	public MealPlanManagerImpl() throws InfraException {
+		fmp = new FactoryMealPlan();
+		persistence = fmp.getPersistence();
+	}
+	
+	@Override
+	public void createMealPlan(String planName, String goals, List<Recipe> recipeList) throws ExceptionMealPlan {
+		MealPlan mp = new MealPlan(planName, new Date(), goals, recipeList);
+	}
+
+	@Override
+	public void updateMealPlan(MealPlan mealPlan, String planName, String goals, List<Recipe> recipeList)
+			throws ExceptionMealPlan {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteMealPlan(MealPlan mealPLan) throws ExceptionMealPlan {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void viewMealPlan(Patient patient) {
+		// TODO Auto-generated method stub
+		
+	}
 }
