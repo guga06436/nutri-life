de74ae7cc4baa32b83ff10cdd7c3e13a63147d6d
Alexandre Bezerra de Lima <53580956+Alexandreprog@users.noreply.github.com>
2023-10-18 00:13:07 -0300

    Alteração do nome das exceções e adição de novas

--- nutri-life/src/controller/AdminManager.java
+++ nutri-life/src/controller/AdminManager.java
@@ -1,13 +1,12 @@
 package controller;
 
-import controller.exceptions.ExceptionEntityNotFound;
-import controller.exceptions.ExceptionPassword;
-import controller.exceptions.ExceptionRegister;
+import controller.exceptions.EntityNotFoundException;
+import controller.exceptions.RegisterException;
 import model.Admin;
 import persistence.db.exception.InfraException;
 
 public interface AdminManager {
-    boolean insert(String name, String email, String username, String password) throws InfraException, ExceptionRegister;
-    Admin retrieve(String username, String password) throws ExceptionEntityNotFound, ExceptionPassword, InfraException;
+    boolean insert(String name, String email, String username, String password) throws InfraException, RegisterException;
+    Admin retrieve(String username, String password) throws EntityNotFoundException, InfraException;
     void generateReport() throws InfraException;
 }
--- nutri-life/src/controller/MealPlanManager.java
+++ nutri-life/src/controller/MealPlanManager.java
@@ -2,14 +2,17 @@ package controller;
 
 import java.util.List;
 
-import controller.exceptions.ExceptionMealPlan;
+import controller.exceptions.DeleteException;
+import controller.exceptions.EntityNotFoundException;
+import controller.exceptions.RegisterException;
+import controller.exceptions.UpdateException;
 import model.MealPlan;
 import model.Patient;
 import model.Recipe;
 
 public interface MealPlanManager {
-    void createMealPlan(String planName, String goals, List<Recipe> recipeList) throws ExceptionMealPlan;
-    void updateMealPlan(MealPlan mealPlan, String planName, String goals, List<Recipe> recipeList) throws ExceptionMealPlan;
-    void deleteMealPlan(MealPlan mealPLan) throws ExceptionMealPlan;
-    void viewMealPlan(Patient patient);
+    void createMealPlan(String planName, String goals, List<Recipe> recipeList) throws RegisterException;
+    void updateMealPlan(MealPlan mealPlan, String planName, String goals, List<Recipe> recipeList) throws UpdateException;
+    void deleteMealPlan(MealPlan mealPLan) throws DeleteException;
+    void retrieve(Patient patient) throws EntityNotFoundException;
 }
