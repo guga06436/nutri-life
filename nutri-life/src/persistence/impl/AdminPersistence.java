35c96236edc42d78d04726e02b44866aaf5e02ae
Ryann <ryann.arruda_159@hotmail.com>
2023-10-18 01:25:30 -0300

    Criação do método retrieve
    Implementação do método retrieve para recuperar uma administrador e do método auxiliar para instanciar um objeto Admin

--- nutri-life/src/persistence/impl/AdminPersistence.java
+++ nutri-life/src/persistence/impl/AdminPersistence.java
@@ -2,6 +2,7 @@ package persistence.impl;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -15,6 +16,16 @@ public class AdminPersistence implements Persistence<Admin>{
 	
 	public AdminPersistence() throws InfraException {
 		conn = Database.getConnection();
+	}
+	
+	private Admin instantiateAdmin(ResultSet rs) throws SQLException {
+		Admin admin = new Admin();
+		
+		admin.setName(rs.getString("admin_name"));
+		admin.setUsername(rs.getString("username"));
+		admin.setPassword(rs.getString("admin_password"));
+		
+		return admin;
 	}
 
 	@Override
@@ -50,9 +61,34 @@ public class AdminPersistence implements Persistence<Admin>{
 	}
 
 	@Override
-	public Admin retrieve(Admin object) {
-		// TODO Auto-generated method stub
-		return null;
+	public Admin retrieve(Admin object) throws InfraException {
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		Admin admin = null;
+		
+		try {
+			ps = conn.prepareStatement("SELECT * FROM SystemAdministrator WHERE username = ? AND admin_password = ?");
+			
+			ps.setString(1, object.getUsername());
+			ps.setString(2, object.getPassword());
+			
+			rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				admin = instantiateAdmin(rs);
+			}
+		}
+		catch(SQLException e) {
+			throw new InfraException("Unable to retrieve an admin");
+		}
+		catch(NullPointerException e) {
+			throw new InfraException("Unable to retrieve an admin: null argument in method call");
+		}
+		finally {
+			Database.closeStatement(ps);
+		}
+		
+		return admin;
 	}
 
 	@Override
@@ -71,6 +107,12 @@ public class AdminPersistence implements Persistence<Admin>{
 	public List<Admin> listAll() throws InfraException {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public int retrieveId(Admin object) throws InfraException {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
