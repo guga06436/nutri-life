3998ac7fc589c48a0a7c4f6d9146a954e5233350
Ryann <ryann.arruda_159@hotmail.com>
2023-10-19 23:59:54 -0300

    Adição do relacionamento entre a refeição e o plano alimentar

--- nutri-life/src/model/Meal.java
+++ nutri-life/src/model/Meal.java
@@ -10,12 +10,46 @@ public class Meal {
 	private String time;
 	private Map<Food, Map<Float, String>> portionedFoods;
 	
-	public Meal(String name, String time, Map<Food, Map<Float, String>> portionedFoods) {
+	private MealPlan mealPlan;
+	
+	public Meal(String name, String time, Map<Food, Map<Float, String>> portionedFoods, MealPlan mealPlan) {
 		this.name = name;
 		this.time = time;
 		this.portionedFoods = portionedFoods;
+		this.mealPlan = mealPlan;
 	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Meal other = (Meal) obj;
+		if (mealPlan == null) {
+			if (other.mealPlan != null)
+				return false;
+		} else if (!mealPlan.equals(other.mealPlan))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((mealPlan == null) ? 0 : mealPlan.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
