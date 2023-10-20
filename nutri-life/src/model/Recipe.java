ccd9203e75d9492a00a2a9be91bbc77ae9007435
Ryann <ryann.arruda_159@hotmail.com>
2023-10-20 03:19:21 -0300

    Adição de relacionamento com a entidade MealPlan e implementação dos métodos, equals, hashCode e toString

--- nutri-life/src/model/Recipe.java
+++ nutri-life/src/model/Recipe.java
@@ -11,14 +11,17 @@ public class Recipe {
 	private Map<Food, Map<Float, String>> portionedIngredients;
 	private List<String> sequenceSteps;
 	
+	private MealPlan mealPlan;
+	
 	public Recipe() {
 		
 	}
 	
-	public Recipe(String name, Map<Food, Map<Float, String>> portionedIngredients, List<String> sequenceSteps) {
+	public Recipe(String name, Map<Food, Map<Float, String>> portionedIngredients, List<String> sequenceSteps, MealPlan mealPlan) {
 		this.name = name;
 		this.portionedIngredients = portionedIngredients;
 		this.sequenceSteps = sequenceSteps;
+		this.mealPlan = mealPlan;
 	}
 	
 	// Adds a new ingredient to the recipe
@@ -29,5 +32,41 @@ public class Recipe {
 	// Adds a new step to the preparation sequence
 	public void addNewStep(String step) {
 		sequenceSteps.add(step);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Recipe other = (Recipe) obj;
+		if (mealPlan == null) {
+			if (other.mealPlan != null)
+				return false;
+		} else if (!mealPlan.equals(other.mealPlan))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((mealPlan == null) ? 0 : mealPlan.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	
+	@Override
+	public String toString() {
+		return name + " (Patient: " + mealPlan.getPatient() + ")";
 	}
 }
