7076276808a082ae751ca7221d0e923e65e99b42
Ryann <ryann.arruda_159@hotmail.com>
2023-10-29 22:16:33 -0300

    Criação de dois atributos de instâncias para representar os logs
    Separação dos logs em arquivos distintos

--- nutri-life/src/service/impl/LogAdapter.java
+++ nutri-life/src/service/impl/LogAdapter.java
@@ -1,25 +1,34 @@
 package service.impl;
 
 import org.apache.log4j.Level;
-import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 import service.LogService;
 
 public class LogAdapter implements LogService{
-	private static final Logger log = LogManager.getLogger(LogAdapter.class);
+	private static LogAdapter logAdapter = null;
+	private static final Logger exceptionLog = Logger.getLogger("exceptionLog");
+	private static final Logger infoLog = Logger.getLogger("infoLog");	
 	
-	public LogAdapter() {
+	private LogAdapter() {
 		
+	}
+	
+	public static LogAdapter getInstance() {
+		if(logAdapter == null) {
+			logAdapter = new LogAdapter();
+		}
+		
+		return logAdapter;
 	}
 	
 	@Override
 	public void logException(Exception e) {
-		log.log(Level.ERROR, e.getMessage());
+		exceptionLog.log(Level.ERROR, e.getMessage());
 	}
 
 	@Override
 	public void logDebug(String message) {
-		log.log(Level.INFO, message);
+		infoLog.log(Level.INFO, message);
 	}
 }
