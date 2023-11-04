833a2485aaa3629af2ca406609edbc9f97aafc67
Alexandre Bezerra de Lima <53580956+Alexandreprog@users.noreply.github.com>
2023-11-04 01:34:39 -0300

    Adição de métodos para realizar operações com o banco de dados

--- nutri-life/src/controller/MealManager.java
+++ nutri-life/src/controller/MealManager.java
@@ -1,24 +1,21 @@
 package controller;
 
+import java.util.List;
+
 import controller.exceptions.DeleteException;
 import controller.exceptions.EntityNotFoundException;
 import controller.exceptions.RegisterException;
 import controller.exceptions.UpdateException;
-import model.Food;
-import model.MealPlan;
 import model.Meal;
 import persistence.db.exception.InfraException;
 
-import java.sql.Time;
-import java.util.List;
-import java.util.Map;
-
 public interface MealManager {
 
-    boolean insert(String name, Map<Food, Map<Float, String>> portionedIngredients, int hour, int minutes, int seconds, MealPlan mealPlan) throws InfraException, RegisterException;
-
-    void updateMeal(Meal meal, String name, Map<Food, Map<Float, String>> portionedIngredients, int hour, int minutes, int seconds) throws UpdateException;
-
+    boolean insert(Meal meal) throws InfraException, RegisterException;
+    boolean updateMeal(Meal meal) throws UpdateException;
     void deleteMeal(Meal meal) throws DeleteException;
-    List<Meal> retrieve(MealPlan mealPlan) throws EntityNotFoundException, InfraException;
+    List<Meal> retrieve(Meal meal) throws EntityNotFoundException, InfraException;
+    int retrieveId(Meal meal);
+    Meal retrieveById(int id);
+    List<Meal> listAll();
 }
