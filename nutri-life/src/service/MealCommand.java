6a402ab717b00c04c4956a18eeaf769da282ae60
Ryann <ryann.arruda_159@hotmail.com>
2023-11-07 00:50:25 -0300

    Criação da interface respeitando o padrão de projeto Command

--- nutri-life/src/service/MealCommand.java
+++ nutri-life/src/service/MealCommand.java
@@ -1,7 +1,9 @@
 package service;
 
+import java.util.List;
+
 import model.Meal;
 
 public interface MealCommand<T>{
-	public T execute(Meal meal) throws Exception;
+	public T execute(List<Meal> meals) throws Exception;
 }
