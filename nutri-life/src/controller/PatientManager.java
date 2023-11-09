0b3151c1e06826974b78b41c4e4bcf542b4cc726
Ryann <ryann.arruda_159@hotmail.com>
2023-11-09 04:32:28 -0300

    Alteração da lista de parâmetros e adição do método retrievePatientNutritionist

--- nutri-life/src/controller/PatientManager.java
+++ nutri-life/src/controller/PatientManager.java
@@ -8,8 +8,9 @@ import persistence.db.exception.InfraException;
 
 public interface PatientManager {
 
-    boolean add(String username , String password, String name, String cpf, int age, float height, float weight, Nutritionist nutritionist) throws InfraException, RegisterException;
+    boolean add(String username , String password, String name, String cpf, int age, float height, float weight) throws InfraException, RegisterException;
 
     void listAll() throws InfraException;
 	Patient retrieve(String username, String password) throws InfraException, EntityNotFoundException;
+	Nutritionist retrievePatientNutritionist(Patient patient) throws InfraException ;
 }
